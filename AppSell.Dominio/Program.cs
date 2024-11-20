a5cb90c236696eb82016a2f48bf38c3358b7f597
Juan Gabriel Gomez Baron <juang77@hotmail.com>
2024-11-20 13:17:05 -0500

    Add project files.

---
+++ AppSell.Dominio/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AppSell.Dominio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
